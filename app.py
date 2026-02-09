7998f176fd7c6defd1593a41d3d060b20ce8a1c9
MarceliBil <marbil003@gmail.com>
2026-02-09 17:02:25 +0100

    Streamlit basic UI add

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from dotenv import load_dotenv
+from rag.rag_chain import get_rag_chain
+
+load_dotenv()
+
+st.set_page_config(page_title="RAG Knowledge Agent")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "chain" not in st.session_state:
+    st.session_state.chain = get_rag_chain()
+
+st.title("RAG Knowledge Agent")
+
+hide_ui = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_ui, unsafe_allow_html=True)
+
+custom_chat_css = """
+<style>
+.st-emotion-cache-khw9fs {
+    background-color: #f2f2f2;
+}
+
+.st-emotion-cache-z68l0b {
+    background-color: rgb(0 207 255);
+}
+
+[data-testid="stChatInput"] > div {
+    border: 1px solid rgba(255, 255, 255, 0.6) !important;
+    box-shadow: none !important;
+    border-radius: 9999px !important;
+}
+
+[data-testid="stChatInput"] > div:focus-within {
+    border: 1px solid #ffffff !important;
+    box-shadow: 0 0 6px rgba(255, 255, 255, 0.4) !important;
+}
+
+.st-emotion-cache-184dg47 {
+    background-color: rgb(0, 173, 64) !important; 
+}
+</style>
+"""
+st.markdown(custom_chat_css, unsafe_allow_html=True)
+
+for m in st.session_state.messages:
+    with st.chat_message(m["role"]):
+        st.markdown(m["content"])
+
+prompt = st.chat_input("Zadaj pytanie na temat zasad w Twojej organizacji...")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.session_state.chain.invoke(prompt)
+        st.markdown(response)
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
